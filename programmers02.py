be4b923f236ab8103c03bfcf261e126dc2430b00
Sieun Bae <qotldms130@gmail.com>
2019-04-17 20:23:14 +0900

    Create programmers02.py

---
+++ programmers02.py
@@ -0,0 +1,11 @@
+###k번째수###
+###level1###
+###sieun Bae###
+def solution(array, commands):
+    answer = []
+    
+    for i in commands:
+        array_2 = array[i[0]-1:i[1]]
+        array_2.sort()
+        answer.append(array_2[i[2]-1])
+    return answer
